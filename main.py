f073bafb373ca77ae8eefb81acec519552154d7f
bruxic99 <86475897+bruxic99@users.noreply.github.com>
2022-05-20 06:37:05 +0200

    adding train unet

--- main.py
+++ main.py
@@ -1,118 +1,154 @@
-import cv2
-import numpy as np
+
+import skimage.io as io
+import skimage.transform as trans
+import tensorflow as tf
 import matplotlib.pyplot as plt
+from PIL import Image
+from tensorflow.python.keras.layers import *
+from tensorflow.python.keras.models import *
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import *
 
 
-def gamma(img_gray, gamma):
-    return (np.power(img_gray / 255, gamma).clip(0, 1) * 255).astype(np.uint8)
+
+Sky = [128,128,128]
+Building = [128,0,0]
+Pole = [192,192,128]
+Road = [128,64,128]
+Pavement = [60,40,222]
+Tree = [128,128,0]
+SignSymbol = [192,128,128]
+Fence = [64,64,128]
+Car = [64,0,128]
+Pedestrian = [64,64,0]
+Bicyclist = [0,128,192]
+Unlabelled = [0,0,0]
+
+COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
+                          Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
+
+def unet(pretrained_weights = None,input_size = (256,256,1)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    drop3 = Dropout(0.5)(conv3)
+
+    conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+
+    up5 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop4))
+    merge5 = concatenate([drop3,up5], axis = 3)
+    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge5)
+    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+
+    up6 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv5))
+    merge6 = concatenate([conv2,up6], axis = 3)
+    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv1,up7], axis = 3)
+    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    conv7 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    conv8 = Conv2D(1, 1, activation = 'sigmoid')(conv7)
+    model = Model(inputs, conv8)
+    model.compile(optimizer="adam", loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
 
 
-def preprocess_image(image):
-    red, green, blue = cv2.split(image)
 
-    green_blured = cv2.GaussianBlur(green, (19, 19), 10)
-    green_sharpened = cv2.addWeighted(green, 1.5, green_blured, -0.5, 0)
+def adjustData(img,mask,flag_multi_class,num_class):
+    img = img / 255.0
+    mask = mask / 255.0
+    print(mask)
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
 
-    contrast_green = cv2.equalizeHist(green_sharpened)
-    return contrast_green
+    return (img,mask)
 
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+    '''
+    can generate image and mask at the same time
+    use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
+    if you want to visualize the results of generator, set save_to_dir = "your path"
+    '''
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    print(train_generator)
+    for (img,mask) in train_generator:
+        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        yield (img,mask)
 
-def process_image(image):
-    kernels = [(5, 5), (11, 11), (23, 23)]
-    morph = image
-    for kernel in kernels:
-        morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel), iterations=1)
-        morph = cv2.morphologyEx(morph, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel), iterations=1)
-
-    substract_image = cv2.subtract(morph, image)
-
-    contrast_substract_image = cv2.equalizeHist(substract_image)
-
-    _, thresh_image = cv2.threshold(contrast_substract_image, 15, 255, cv2.THRESH_BINARY)
-
-    return contrast_substract_image, thresh_image
-
-
-def post_process_image(image, tresh):
-    mask = np.ones(image.shape[:2], dtype="uint8") * 255
-    contours, hierarchy = cv2.findContours(tresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    for cnt in contours:
-        if cv2.contourArea(cnt) <= 200:
-            cv2.drawContours(mask, [cnt], -1, 0, -1)
-
-    im = cv2.bitwise_and(image, image, mask=mask)
-
-    _, thresh_im = cv2.threshold(im, 15, 255, cv2.THRESH_BINARY_INV)
-
-    eroded = cv2.erode(thresh_im, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=1)
-
-    blood_vessels = cv2.bitwise_not(eroded)
-
-    return blood_vessels
-
-
-def statistic(original, result, vessels):
-    mask = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-    mask = gamma(mask, 1.5)
-    _, mask = cv2.threshold(mask, 15, 255, cv2.THRESH_BINARY)
-    mask = cv2.bitwise_not(mask)
-
-    noice = result - cv2.bitwise_not(mask)
-    noice = cv2.dilate(noice, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=1)
-    noice = cv2.bitwise_not(noice)
-
-    result = cv2.bitwise_and(result, noice)
-
-    false_positive = result - vessels
-    false_negative = vessels - result
-    true_positive = cv2.bitwise_and(result, vessels)
-    true_negative = cv2.bitwise_not(result + vessels) - mask
-
-    FP = np.sum(false_positive == 255)
-    FN = np.sum(false_negative == 255)
-    TP = np.sum(true_positive == 255)
-    TN = np.sum(true_negative == 255)
-
-    acurracy = (TP + TN) / (TP + TN + FP + FN)
-    sensitivity = TP / (TP + FN)
-    specificity = TN / (TN + FP)
-
-    return result, [acurracy, sensitivity, specificity]
+def testGenerator(test_path,num_image = 15,target_size = (256,256),flag_multi_class = False,as_gray = True):
+    for i in range(1, num_image+1):
+        if i < 10:
+            img = io.imread(os.path.join(test_path,"0%d_h.jpg"%i),as_gray = as_gray)
+        else:
+            img = io.imread(os.path.join(test_path,"%d_h.jpg"%i),as_gray = as_gray)
+        img = img/ 255.0
+        img = trans.resize(img,target_size)
+        img = np.reshape(img,img.shape+(1,))
+        img = np.reshape(img,(1,)+img.shape)
+        yield img
 
 
-filename_eye = str(input("Podaj nazwę pliku zawierającego zdjęcie oka: "))
-filename_vessel = str(input("Podaj nazwę pliku zawierającego zdjęcie naczynek: "))
 
-filename_eye = "./eye/" + filename_eye + ".jpg"
-filename_vessel = "./vessels/" + filename_vessel + ".tif"
+def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
+    for i,item in enumerate(npyfile):
+        img = item * 255
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
 
-print(filename_eye)
-img = cv2.imread("zdjecia_oczy/"+filename_eye)
-vessels = cv2.imread("zdjecia_naczynka/"+filename_vessel)
 
-if img is not None and vessels is not None:
-    vessels = cv2.cvtColor(vessels, cv2.COLOR_BGR2GRAY)
-    original = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    original = cv2.resize(original, (700, 605))
-    vessels = cv2.resize(vessels, (700, 605))
+if __name__ == '__main__':
 
-    preimage = preprocess_image(original)
-    image, tresh = process_image(preimage)
-    result = post_process_image(image, tresh)
-    end_image, stats = statistic(original, result, vessels)
+    data_gen_args = dict(rotation_range=0.2,
+                        width_shift_range=0.05,
+                        height_shift_range=0.05,
+                        shear_range=0.05,
+                        zoom_range=0.05,
+                        horizontal_flip=True,
+                        fill_mode='nearest')
+    myGene = trainGenerator(2,'./','eye','vessels',data_gen_args,save_to_dir = None)
+    model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
 
-    plt.title("Wykryte naczynka oka")
-    plt.imshow(end_image, cmap="gray")
-    plt.show()
-    plt.title("Rzeczywiste naczynka oka")
-    plt.imshow(vessels, cmap="gray")
-    plt.show()
+    model = unet()
+    #model.fit_generator(myGene,steps_per_epoch=20,epochs=5,callbacks=[model_checkpoint])
 
-    print("Trafność: {} \nCzułość: {}, \nSwoistość: {}.".format(stats[0], stats[1], stats[2]))
-elif img is None and vessels is not None:
-    print("Plik zawierający zdjęcie oka nie istnieje.")
-elif img is not None and vessels is None:
-    print("Plik zawierający zdjęcie naczynek nie istnieje.")
-else:
-    print("Podane pliki nie istnieją.")
+    #testGene = testGenerator("eye")
+    #results = model.predict_generator(testGene,15,verbose=1)
+    #saveResult("results",results)
+
