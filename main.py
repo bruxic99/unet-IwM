13a30962815e0ef66d055d69555c2e447c610907
bruxic99 <k.kopciuch99@gmail.com>
2022-06-11 17:55:43 +0200

    add test

--- main.py
+++ main.py
@@ -8,147 +8,28 @@ from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.models import *
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import *
+from skimage import img_as_uint
 
 
-
-Sky = [128,128,128]
-Building = [128,0,0]
-Pole = [192,192,128]
-Road = [128,64,128]
-Pavement = [60,40,222]
-Tree = [128,128,0]
-SignSymbol = [192,128,128]
-Fence = [64,64,128]
-Car = [64,0,128]
-Pedestrian = [64,64,0]
-Bicyclist = [0,128,192]
-Unlabelled = [0,0,0]
-
-COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
-                          Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
-
-def unet(pretrained_weights = None,input_size = (256,256,1)):
-    inputs = Input(input_size)
-    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    drop3 = Dropout(0.5)(conv3)
-
-    conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-
-    up5 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop4))
-    merge5 = concatenate([drop3,up5], axis = 3)
-    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge5)
-    conv5 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-
-    up6 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv5))
-    merge6 = concatenate([conv2,up6], axis = 3)
-    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-    up7 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv1,up7], axis = 3)
-    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-    conv7 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-    conv8 = Conv2D(1, 1, activation = 'sigmoid')(conv7)
-    model = Model(inputs, conv8)
-    model.compile(optimizer="adam", loss = 'binary_crossentropy', metrics = ['accuracy'])
-    return model
-
-
-
-def adjustData(img,mask,flag_multi_class,num_class):
-    img = img / 255.0
-    mask = mask / 255.0
-    print(mask)
-    mask[mask > 0.5] = 1
-    mask[mask <= 0.5] = 0
-
-    return (img,mask)
-
-def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
-                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
-                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
-    '''
-    can generate image and mask at the same time
-    use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
-    if you want to visualize the results of generator, set save_to_dir = "your path"
-    '''
-    image_datagen = ImageDataGenerator(**aug_dict)
-    mask_datagen = ImageDataGenerator(**aug_dict)
-    image_generator = image_datagen.flow_from_directory(
-        train_path,
-        classes = [image_folder],
-        class_mode = None,
-        color_mode = image_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = image_save_prefix,
-        seed = seed)
-    mask_generator = mask_datagen.flow_from_directory(
-        train_path,
-        classes = [mask_folder],
-        class_mode = None,
-        color_mode = mask_color_mode,
-        target_size = target_size,
-        batch_size = batch_size,
-        save_to_dir = save_to_dir,
-        save_prefix  = mask_save_prefix,
-        seed = seed)
-    train_generator = zip(image_generator, mask_generator)
-    print(train_generator)
-    for (img,mask) in train_generator:
-        img,mask = adjustData(img,mask,flag_multi_class,num_class)
-        yield (img,mask)
-
-def testGenerator(test_path,num_image = 15,target_size = (256,256),flag_multi_class = False,as_gray = True):
-    for i in range(1, num_image+1):
-        if i < 10:
-            img = io.imread(os.path.join(test_path,"0%d_h.jpg"%i),as_gray = as_gray)
+def testGenerator(train_path, target_size=(512,512)):
+    for i in range(1, 19):
+        if i<10:
+            img = io.imread(os.path.join(train_path,"0%d_h.jpg"%i))
         else:
-            img = io.imread(os.path.join(test_path,"%d_h.jpg"%i),as_gray = as_gray)
-        img = img/ 255.0
+            img = io.imread(os.path.join(train_path,"%d_h.jpg"%i))
         img = trans.resize(img,target_size)
-        img = np.reshape(img,img.shape+(1,))
         img = np.reshape(img,(1,)+img.shape)
         yield img
 
 
-
-def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
+def saveResult(save_path, npyfile):
     for i,item in enumerate(npyfile):
-        img = item * 255
-        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
-
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img_as_uint(item))
 
 
 if __name__ == '__main__':
-
-    data_gen_args = dict(rotation_range=0.2,
-                        width_shift_range=0.05,
-                        height_shift_range=0.05,
-                        shear_range=0.05,
-                        zoom_range=0.05,
-                        horizontal_flip=True,
-                        fill_mode='nearest')
-    myGene = trainGenerator(2,'./','eye','vessels',data_gen_args,save_to_dir = None)
-    model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-
-    model = unet()
-    #model.fit_generator(myGene,steps_per_epoch=20,epochs=5,callbacks=[model_checkpoint])
-
-    #testGene = testGenerator("eye")
-    #results = model.predict_generator(testGene,15,verbose=1)
-    #saveResult("results",results)
+    model = load_model('unet_eye4.hdf5')
+    testGene = testGenerator("eye")
+    results = model.predict(testGene, steps=18, verbose=1)
+    saveResult("results", results)
 
